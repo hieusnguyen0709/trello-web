01e347798e1a251476b76e7e1530a737705187a3
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2025-08-16 09:51:24 +0700

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Button from '@mui/material/Button';
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 import { pink } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <>
       <div>HieuNM</div>
-      <Button variant="contained">Hello world</Button>
+      <Typography variant="body2" color="text.secondary">Test Typography</Typography>
+      <Button variant="contained" color="success">Hello world</Button>
       <AccessAlarm/>
       <ThreeDRotation sx={{ color: pink[500] }}/>
     </>
