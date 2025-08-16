12e5d175182c1623f9475301e8d9e655bc387986
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2025-08-16 10:30:27 +0700

    add extend theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,26 @@ import Button from '@mui/material/Button';
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 import { pink } from '@mui/material/colors';
 import Typography from '@mui/material/Typography';
+import { useColorScheme } from '@mui/material/styles';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
 
 function App() {
   return (
     <>
+      <ModeToggle/>
+      <hr/>
       <div>HieuNM</div>
       <Typography variant="body2" color="text.secondary">Test Typography</Typography>
       <Button variant="contained" color="success">Hello world</Button>
