e658f289a02bbf4b17a3c7464736e16914e95ace
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2025-09-06 11:09:37 +0700

    fix UI UX

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,14 +3,14 @@ import Container from '@mui/material/Container';
 import AppBar from '~/components/AppBar/AppBar';
 import BoardBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
-// import { mockData } from '~/apis/mock-data';
+import { mockData } from '~/apis/mock-data';
 import { fetchBoardDetailsAPI } from '~/apis';
 
 function Board() {
   const [board, setBoard] = useState(null)
 
   useEffect(() => {
-    const boardId = '68b906d84c467fd1e9118e82'
+    const boardId = '68b906d84c467fd1e9118e81'
     fetchBoardDetailsAPI(boardId).then(board => {
       setBoard(board)
     })
@@ -19,8 +19,8 @@ function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar/>
-      <BoardBar board={board}/>
-      <BoardContent board={board}/>
+      <BoardBar board={mockData.board}/>
+      <BoardContent board={mockData.board}/>
     </Container>
   );
 }
