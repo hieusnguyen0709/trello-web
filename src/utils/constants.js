ff780417722f76b787cde4ecb33265d362a7a90b
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2025-09-22 15:40:13 +0700

    update domain API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ if (process.env.BUILD_MODE === 'dev') {
     apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-    apiRoot = 'https://trello-api-mxyn.onrender.com/'
+    apiRoot = 'https://trello-api-mxyn.onrender.com'
 }
 export const API_ROOT = apiRoot
