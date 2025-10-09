4f78ea93521161c8b144ead2e7df258304d7c98e
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2025-10-09 11:39:13 +0700

    update auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
 import Board from "~/pages/Boards/_id";
+import NotFound from './pages/404/NotFound';
+import Auth from './pages/Auth/Auth';
 
 function App() {
   return (
-    <>
-      <Board/>
-    </>
+    <Routes>
+      <Route path='/' element={
+        <Navigate to='boards/68b906d84c467fd1e9118e82' replace={true} />
+      } />
+
+      <Route path='/boards/:boardId' element={<Board/>} />
+
+      <Route path='/login' element={<Auth/>} />
+      <Route path='/register' element={<Auth/>} />
+
+      <Route path='*' element={<NotFound/>} />
+    </Routes>
   );
 }
 
