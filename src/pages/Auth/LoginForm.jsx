6cb4db5d9b0d3208a2130c6dda72d05194de2358
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2026-01-13 12:01:06 +0700

    upload attachment file

--- src/pages/Auth/LoginForm.jsx
+++ src/pages/Auth/LoginForm.jsx
@@ -59,7 +59,7 @@ function LoginForm() {
             <Avatar sx={{ bgcolor: 'primary.main' }}><TrelloIcon /></Avatar>
           </Box>
           <Box sx={{ marginTop: '1em', display: 'flex', justifyContent: 'center', color: theme => theme.palette.grey[500] }}>
-            Author: HieuNM
+            LOGIN
           </Box>
           <Box sx={{ marginTop: '1em', display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '0 1em' }}>
             { verifiedEmail && 
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -13,15 +13,48 @@ export const PASSWORD_CONFIRMATION_MESSAGE = 'Password Confirmation does not mat
 // Liên quan đến Validate File
 export const LIMIT_COMMON_FILE_SIZE = 10485760 // byte = 10 MB
 export const ALLOW_COMMON_FILE_TYPES = ['image/jpg', 'image/jpeg', 'image/png']
-export const singleFileValidator = (file) => {
+export const LIMIT_COMMON_ATTACHMENT_FILE_SIZE = 10485760 // byte = 10 MB
+export const ALLOW_COMMON_ATTACHMENT_FILE_TYPES = [
+  'application/pdf',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.ms-excel',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+]
+
+export const singleFileValidator = (file, type) => {
   if (!file || !file.name || !file.size || !file.type) {
     return 'File cannot be blank.'
   }
-  if (file.size > LIMIT_COMMON_FILE_SIZE) {
-    return 'Maximum file size exceeded. (10MB)'
+  
+  if (type == 'image') {
+    if (file.size > LIMIT_COMMON_FILE_SIZE) {
+      return 'Maximum file size exceeded. (10MB)'
+    }
+    if (!ALLOW_COMMON_FILE_TYPES.includes(file.type)) {
+      return 'File type is invalid. Only accept jpg, jpeg and png'
+    }
+  } else if (type == 'file') {
+    if (file.size > LIMIT_COMMON_ATTACHMENT_FILE_SIZE) {
+      return 'Maximum file size exceeded. (10MB)'
+    }
+    if (!ALLOW_COMMON_ATTACHMENT_FILE_TYPES.includes(file.type)) {
+      return 'File type is invalid. Only accept PDF (.pdf), Word (.doc, .docx), Excel (.xls, .xlsx)'
+    }
   }
-  if (!ALLOW_COMMON_FILE_TYPES.includes(file.type)) {
-    return 'File type is invalid. Only accept jpg, jpeg and png'
+
+  return null
+}
+
+export const multiFilesValidator = (files, type) => {
+  if (!files || files.length === 0) {
+    return 'Please select at least one file.'
   }
+
+  for (const file of files) {
+    const error = singleFileValidator(file, type)
+    if (error) return error
+  }
+
   return null
 }
