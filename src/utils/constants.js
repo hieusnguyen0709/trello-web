abe9a28ff21010e9938780289873ffb1b79a8749
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2026-08-05 16:47:56 +0700

    update apiRoot in production env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-mxyn.onrender.com'
+  apiRoot = process.env.VITE_API_ROOT
 }
 export const API_ROOT = apiRoot
 
