8008717a19dbf2e1459a1321eaaad93273bfb363
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2026-08-05 20:00:30 +0700

    test

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = process.env.VITE_API_ROOT
+  apiRoot = 'http://100.53.216.68:8017'
 }
 export const API_ROOT = apiRoot
 
